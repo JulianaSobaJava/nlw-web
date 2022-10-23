7a7f8be109641b2df5d421e6ff108df4c2690f59
JulianaSobaJava <julianasobajava23@gmail.com>
2022-10-23 20:58:22 +0100

    :sparkles: create GameBanner and CreateAdd components

---
+++ src/components/createAdBanner.tsx
@@ -0,0 +1,25 @@
+// interface GameProps {
+//     imageUrl: string;
+//     title: string;
+//     adsCount: number;
+// }
+
+import { MagnifyingGlassPlus } from 'phosphor-react';
+
+export function CreateAdBanner() {
+    return (
+        <div className='self-stretch bg-nlw-gradient pt-1 mt-6 rounded-lg overflow-hidden'>
+            <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+                <div>
+                    <strong className="text-2xl text-white font-black block">Não encntrou o seu duo?</strong>
+                    <span className='text-zinc-400 text-sm block'>Publique um anúncio para encontrar novos players!</span>
+                </div>
+
+                <button className='py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3'>
+                    <MagnifyingGlassPlus size={24} />
+                    Publicar anúncios</button>
+            </div>
+
+        </div>
+    )
+}
